244b56ce5480e77f9d7c036def00fb3c352e1150
Maciej Plonski <m.plonski@sokoli.pl>
2014-05-08 03:27:44 +0200

    not anymore

--- 2048.sh
+++ 2048.sh
@@ -10,7 +10,7 @@
 # table used for keeping values from game
 arr=()
 
-# colors - TODO
+# colors
 RED='\e[1;31m' ; GREEN='\e[1;32m' ; YELLOW='\e[1;33m' ; BLUE='\e[1;34m' ; MAGENTA='\e[1;35m' ; CYAN='\e[1;36m' NOR='\e[m'
 
 # print using colors
